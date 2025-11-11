e431635d96b603b704cbe81b23533148ce7176fe
Boniface Hillary <nkasiboniface@gail.com>
2025-11-11 01:01:26 +0100

    initial commit

--- game.js
+++ game.js
@@ -36,7 +36,6 @@ let questions = [];
   //     "what is the correct syntax for refering to an external script called 'xxx.js'?",
   //   choice1: "<script href='xxx.js'>",
   //   choice2: "<script name='xxx.js'>",
-  //   choice3:
   // },
   
   // },  
